29b7e803a499e64e3e2b4d90ee9ca1e92f29063f
Mateus Ribeiro <mateus_ribeiro21@hotmail.com>
2024-04-09 20:33:38 -0300

    feat: fetch products for home page

---
+++ src/env.ts
@@ -0,0 +1,14 @@
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NEXT_PUBLIC_API_BASE_URL: z.string().url(),
+})
+
+const parsedEnv = envSchema.safeParse(process.env)
+
+if (!parsedEnv.success) {
+  console.error('Invalid env variables', parsedEnv.error.flatten().fieldErrors)
+  throw new Error('Invalid env variables')
+}
+
+export const env = parsedEnv.data
